760a8627838de1854ab32d3b4daf0912429adbc7
Victor Macari <victorm.macari@gmail.com>
2024-10-02 18:01:57 +0300

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Victor Macari on 02.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
